5eae20778aa9ba95b4f51632cac7daface57b47b
chechna9 <ja_bouchouareb@esi.dz>
2022-05-28 10:09:39 +0100

    fixing delete

--- simple_app/lib/home.dart
+++ simple_app/lib/home.dart
@@ -28,7 +28,7 @@ class _HomeState extends State<Home> {
 
   void delete(String number) {
     setState(() {
-      contacts.remove((element) => element.phoneNumber == number);
+      contacts.removeWhere((element) => element.phoneNumber == number);
     });
   }
 
